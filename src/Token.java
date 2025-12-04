a096c5acc36f39dd4231cfe5172e802ff51edef8
Ron Xavier <Ronelxavi@gmail.com>
2025-12-04 13:34:01 -0500

    Fixed wrong import statement
    You can now go through most of the operators, braces and parenthesis, and the scanner will spit out tokens!

--- src/Token.java
+++ src/Token.java
@@ -12,5 +12,7 @@ public class Token {
 
     public String toString() {
         return type + " " + lexeme + " " + literal;
+        /* I clearly don't know enough about java because I just found out you can provide instructions on how
+        to convert objects into readable strings by giving the class a toString() method.*/
     }
 }
